085943f3558dc68389043e2a869170589135b3a7
Mary Gardiner <mary@mary.gardiner.id.au>
2012-08-04 09:08:10 +1000

    Add MIT licence, add LCA_ prefix to functions to help avoid NS collisions

--- LCA_assistance_hiding/LCA_assistance_hiding.user.js
+++ LCA_assistance_hiding/LCA_assistance_hiding.user.js
@@ -6,7 +6,17 @@
 // @version     1
 // ==/UserScript==
 
-function checkEm(p, text) {
+/* 
+
+Copyright (c) 2012 Mary Gardiner
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+
+function LCA_checkEm(p, text) {
 /* This is horrible: if Zookeepr made more liberal (any) use of id
    attributes, we could find them directly by use of
    getElementById */
@@ -18,17 +28,17 @@ function checkEm(p, text) {
 	}
 }
 
-var assistanceStrings = {"Accommodation assistance:" : "accom",
+var LCA_assistanceStrings = {"Accommodation assistance:" : "accom",
 		"Travel assistance:" : "travel"};
 
-function findPs() {
+function LCA_findPs() {
 	var ps = document.getElementsByTagName("p");
 	var myPs = {};
 	for (i = 0; i < ps.length; i++) {
 		var p = ps[i];
-		for (var searchString in assistanceStrings) {
-			if (checkEm(p, searchString)) {
-				var suffix = assistanceStrings[searchString];
+		for (var searchString in LCA_assistanceStrings) {
+			if (LCA_checkEm(p, searchString)) {
+				var suffix = LCA_assistanceStrings[searchString];
 				myPs[suffix] = p;
 			}
 		}
@@ -36,55 +46,55 @@ function findPs() {
 	return myPs;
 }
 
-function getAssistanceId(suffix) {
+function LCA_getAssistanceId(suffix) {
 	return "unhidep-" + suffix;
 }
 
-function createUnhide(idsuffix) {
+function LCA_createUnhide(idsuffix) {
         var unhidePElem = document.createElement("div");
 	unhidePElem.style.border = "thin solid #ff0";
 	unhidePElem.style.backgroundColor = "#ffc";
 	unhidePElem.style.padding = "1ex";
 	unhidePElem.style.fontWeight = "bold";
 	unhidePElem.style.textDecoration = "underline";
-	unhidePElem.id = getAssistanceId(idsuffix);
+	unhidePElem.id = LCA_getAssistanceId(idsuffix);
 	return unhidePElem;
 }
 
-function createUnhides() {
-	var ps = findPs();
+function LCA_createUnhides() {
+	var ps = LCA_findPs();
 	for (var key in ps) {
 		var p = ps[key];
-		var unhide = createUnhide(key);
+		var unhide = LCA_createUnhide(key);
 		p.parentNode.insertBefore(unhide, p);
 	}
 }
 
-function hideP(suffix) {
-	var unhide = document.getElementById(getAssistanceId(suffix));
-	var ps = findPs();
+function LCA_hideP(suffix) {
+	var unhide = document.getElementById(LCA_getAssistanceId(suffix));
+	var ps = LCA_findPs();
 	var p = ps[suffix];
 	p.style.visibility = "hidden";
 	unhide.textContent = "Hidden " + suffix + " assistance request status, click to see.";
-	unhide.onclick = function () {showP(suffix);};
+	unhide.onclick = function () {LCA_showP(suffix);};
 
 	return false;
 }
 
-function showP(suffix) {
-	var ps = findPs();
+function LCA_showP(suffix) {
+	var ps = LCA_findPs();
 	var p = ps[suffix];
 	p.style.visibility = "visible";
 	var unhidePElem = document.getElementById(
-			getAssistanceId(suffix));
+			LCA_getAssistanceId(suffix));
 	unhidePElem.textContent = "Showing " + suffix + " assistance request status, click to hide.";
 	unhidePElem.onclick = function () {
-		hideP(suffix); };
+		LCA_hideP(suffix); };
 	return false;
 }
 
-createUnhides();
-for (var searchString in assistanceStrings) {
-	var suffix = assistanceStrings[searchString];
-	hideP(suffix);
+LCA_createUnhides();
+for (var searchString in LCA_assistanceStrings) {
+	var suffix = LCA_assistanceStrings[searchString];
+	LCA_hideP(suffix);
 }
--- LCA_reviewer_hiding/LCA_reviewer_hiding.user.js
+++ LCA_reviewer_hiding/LCA_reviewer_hiding.user.js
@@ -6,7 +6,18 @@
 // @version     1
 // ==/UserScript==
 
-function checkTable(table) {
+
+/*
+Copyright (c) 2012 Mary Gardiner
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+function LCA_checkTable(table) {
 /* This is horrible: if Zookeepr made more liberal (any) use of id
    attributes, we could find them directly by use of
    getElementById */
@@ -16,18 +27,18 @@ function checkTable(table) {
 	return firstTh.textContent == "# - Reviewer";
 }
 
-function findTable() {
+function LCA_findTable() {
 	var tables = document.getElementsByTagName("table");
 
 	for (i = 0; i < tables.length; i++) {
 		var tab = tables[i];
-		if (checkTable(tab)) {
+		if (LCA_checkTable(tab)) {
 			return tab;
 		}
 	}
 }
 
-function createUnhide() {
+function LCA_createUnhide() {
         var unhideTableElem = document.createElement("div");
 	unhideTableElem.style.border = "thin solid #ff0";
 	unhideTableElem.style.backgroundColor = "#ffc";
@@ -35,28 +46,28 @@ function createUnhide() {
 	unhideTableElem.style.fontWeight = "bold";
 	unhideTableElem.style.textDecoration = "underline";
 	unhideTableElem.id = "unhidetable";
-	table = findTable();
+	table = LCA_findTable();
 	table.parentNode.insertBefore(unhideTableElem, table);
 	return unhideTableElem;
 }
 
-function hideTable() {
-	table = findTable();
+function LCA_hideTable() {
+	table = LCA_findTable();
 	table.style.visibility = "hidden";
 	var unhide = document.getElementById("unhidetable");
 	unhide.textContent = "Existing reviews hidden, click to see.";
-	unhide.onclick = showTable;
+	unhide.onclick = LCA_showTable;
 	return false;
 }
 
-function showTable() {
-	table = findTable();
+function LCA_showTable() {
+	table = LCA_findTable();
 	table.style.visibility = "visible";
 	var unhideTableElem = document.getElementById("unhidetable");
 	unhideTableElem.textContent = "Showing existing reviews, click to hide.";
-	unhideTableElem.onclick = hideTable;
+	unhideTableElem.onclick = LCA_hideTable;
 	return false;
 }
 
-createUnhide();
-hideTable();
+LCA_createUnhide();
+LCA_hideTable();
